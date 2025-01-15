f1bb7cf12913a9b3c867a1ef21845752ebe6d271
= <=>
2025-01-14 22:44:16 -0500

    Add audio file import

--- src/app.cpp
+++ src/app.cpp
@@ -6,20 +6,40 @@
 #include "imgui.h"
 #include "implot.h"
 #include "app.h"
+#include "tinywav.h"
 
 using namespace DSPPlotter;
 
+#define INPUT_FILE_PATH "audio/bass_jet.wav"
+#define BUFFER_SIZE 512 // In samples per channel, used while loading file
+
 void App::init() {
-    printf("Plotting App\n");
-    inputData.resize(1000);
-    outputData.resize(1000);
-    for (uint32_t i = 0; i < inputData.size(); i++) {
-        inputData[i] = sinf((float) i / 4.f);
-        outputData[i] = 0.6f * sinf((float) i / 4.5f);
+    // Load audio file
+    TinyWav tw;
+    tinywav_open_read(&tw, INPUT_FILE_PATH, TW_SPLIT);
+    inputData.resize(tw.numFramesInHeader);
+    float buf[tw.numChannels * BUFFER_SIZE];
+    float * samplePtrs[tw.numChannels];
+    for (int chan = 0; chan < tw.numChannels; chan++) {
+        samplePtrs[chan] = buf + chan * BUFFER_SIZE;
+    }
+    
+    uint32_t channelToRead = 0;
+    uint32_t writeIdx = 0;
+    while (1) {
+        int numRead = tinywav_read_f(&tw, samplePtrs, BUFFER_SIZE);        
+        memcpy(&inputData[writeIdx += numRead], samplePtrs[channelToRead], numRead * sizeof(float));
+        if (numRead < BUFFER_SIZE) break;
+    }
+
+    // Do some processing (replace with DUT)
+    outputData.resize(inputData.size());
+    for (uint32_t i = 0; i < outputData.size(); i++) {
+        outputData[i] = fabsf(inputData[i]) - 0.5; // ABS distortion
     }
 
     // Configure
-    cfg.inOutOverlap = true;
+    cfg.inOutOverlap = false;
     return;
 }
 
